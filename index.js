c653c0d4a0ef0912457ebb78c234aed0d9bbaf92
Eric E <ethridge2004@gmail.com>
2020-12-28 13:39:48 -0500

    chunda from the downunder!

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.set('trust proxy', true);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello (H0la) From App Engine.!');
 });
 
+app.get('/submit', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/form.html'));
+});
 const server = app.listen(8080, () => {
   const host = server.address().address;
   const port = server.address().port;
