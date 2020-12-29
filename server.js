d399bec7c31a2f987af609a33e8b32fa5511e79c
Eric E <ethridge2004@gmail.com>
2020-12-28 22:24:11 -0500

    fixing that stuff

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require ('path')
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', (req, res) => {
   res.send('Hello from App Engine!');
+});
+
+app.get('/submit', (req,res) => {
+  res.sendFile(path.join(__dirname, 'views/form.html'));
+});
+
+app.post('/submit', (req, res) => {
+  console.log({
+    name: req.body.name,
+    message: req.body.message
+  });
+  res.send('We have taken your message to the deep deep, thank you');
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
@@ -10,3 +27,5 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
+
+module.exports = app;
